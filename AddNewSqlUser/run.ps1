730fea69aa8209e1b386704505507886605bbbeb
Máté Bányi <banyimatex@gmail.com>
2023-11-07 10:34:15 +0100

    update to function v4

--- AddNewSqlUser/run.ps1
+++ AddNewSqlUser/run.ps1
@@ -15,25 +15,51 @@ $connectionString = 'Data Source={0};database={1};User ID={2};Password={3}' -f $
 if( 
     $Request.Body -and
     (-not [string]::IsNullOrEmpty($Request.Body.username)) -and
-    (-not [string]::IsNullOrEmpty($Request.Body.password)) 
+    (-not [string]::IsNullOrEmpty($Request.Body.password)) -and
+    (-not [string]::IsNullOrEmpty($Request.Body.database))
 ){
-    $query = "CREATE LOGIN {0} WITH PASSWORD = '{1}';" -f $Request.Body.username,$Request.Body.password
+    $query = "CREATE LOGIN {0} WITH PASSWORD = '{1}'; CREATE USER {0} FOR LOGIN {0} WITH DEFAULT_SCHEMA=[dbo];" -f $Request.Body.username,$Request.Body.password
     $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $connectionString
     $command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $sqlConnection)
-    
+
+    $connectionString2 = 'Data Source={0};database={1};User ID={2};Password={3}' -f $ServerName,$Request.Body.database,$userName,$password
+    $query2 = "CREATE USER {0} FOR LOGIN {0} WITH DEFAULT_SCHEMA=[dbo];ALTER ROLE db_owner ADD MEMBER {0};" -f $Request.Body.username
+    $sqlConnection2 = New-Object System.Data.SqlClient.SqlConnection $connectionString2
+    $command2 = New-Object -TypeName System.Data.SqlClient.SqlCommand($query2, $sqlConnection2)
+    $hiba = $false
+    Try
+    {
     $sqlConnection.Open()
     $command.ExecuteNonQuery()
     $sqlConnection.Close()
 
+    $sqlConnection2.Open()
+    $command2.ExecuteNonQuery()
+    $sqlConnection2.Close()
+    }
+    Catch
+    {
+        $hiba=$true 
+        $message = $_
+        Write-Warning "Something happened! $message"
+    }
+    if($hiba -eq $true)
+    {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::BadRequest
+        Body = 'Jelszó nem megfelelő'})
+    }
+    else
+    {
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
-        Body = 'ok'
-    })
+        Body = 'ok'})
+    }
 }
 else{
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::BadRequest
-        Body = 'fail'
+        Body = 'Hiányos paraméterek'
     })
 }
 
